218d6ad5c091918b823fe218de27d18850addb27
jfang <im.jonathan.fang@gmail.com>
2026-09-15 19:18:29 -0700

    2.3 list files finally works

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,8 +1,14 @@
 import os # need to import os i guess
+# from collections.abc import Iterator # apparently isn't necessary for map?
 
 def get_files_info(working_directory: str, directory: str = ".") -> str:
 
     try:
+        if directory == ".":
+            print(f"Result for current directory:")
+        else:
+            print(f"Result for '{directory}' directory:")
+
         working_dir_abs: str = os.path.abspath(working_directory) # was abs_working_directory
         target_dir: str = os.path.normpath(os.path.join(working_dir_abs, directory)) # was abs_working_directory
 
@@ -10,13 +16,36 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
 
         if not valid_target_dir:
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            return f'    Error: Cannot list "{directory}" as it is outside the permitted working directory'
         
         if not os.path.isdir(target_dir):
-            return f'Error: "{directory}" is not a directory'
+            return f'    Error: "{directory}" is not a directory'
 
-        if target_dir:
-            return f'Success: "{directory}" is within the working directory'
+        apparent_list: list = (os.listdir(target_dir)) #how was i supposed to know that this would give a list of file names?
+
+        # if target_dir:
+        # return f'Success: "{directory}" is within the working directory'
+        
+        # iterate over items in target_dir
+
+        def map_helper(item_name: str) -> str:
+            # print(f'whats target dir: {target_dir}')
+            # name: str = os.listdir(target_dir) #name, apparently this isn't the name, its a list of filenames?
+            # print(f'item name apparently {item_name}')
+
+            full_path = os.path.join(target_dir, item_name)
+            
+            file_size: int = os.path.getsize(full_path) # file size in bytes
+            is_dir: bool = os.path.isdir(full_path) #is_dir bool
+            # return name, file_size, is_dir
+            return f'  - {item_name}: file_size={file_size} bytes, is_dir={is_dir}'
+        what_now = list(map(map_helper, apparent_list))
+
+        # print(what_now) # is a list of str items
+
+        return "\n".join(what_now)
+
+        # return f'- {name}: file_size={file_size}, is_dir={is_dir}'
     
     except Exception as e:
-        return f'Error: {e}'
+        return f'    Error: {e}'
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,11 +1,7 @@
 from functions.get_files_info import get_files_info
 
+# print('Result for current directory:')
 print(get_files_info("calculator", "."))
+print(get_files_info("calculator", "pkg"))
 print(get_files_info("calculator", "/bin"))
 print(get_files_info("calculator", "../"))
-print(get_files_info("calculator", "main.py"))
-
-# return get_files_info("calculator", ".")
-# return get_files_info("calculator", "/bin")
-# return get_files_info("calculator", "../")
-# return get_files_info("calculator", "main.py")
